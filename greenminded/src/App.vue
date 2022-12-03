5e08679d099e829398603b32da80b31378ec5d70
Clément CHAZALET <clementchazalet@gmail.com>
2022-12-03 10:48:01 +0100

    Header with Footer

--- greenminded/src/App.vue
+++ greenminded/src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
 	name: 'App',
 	components: {
-		Header
+		Header,
+		Footer
 	}
 }
 </script>
 
 <template>
 	<Header/>
+	<Footer/>
 </template>
--- greenminded/src/components/Header.vue
+++ greenminded/src/components/Header.vue
@@ -1,33 +1,87 @@
-//src/components/Navbar.vue
 <template>
-    <div id="app">
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div class="container">
-				<a class="navbar-brand"><img src="../../img/logo.png"
-									alt="logo"/></a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-					<ul class="navbar-nav ms-auto">
-						<li class="nav-item">
-							<a class="nav-link active" href="">Accueil</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="">Connexion</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="">A propos</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</div>
+  <nav class="navbar navbar-expand-sm navbar-light" id="neubar">
+    <div class="container">
+      <a class="navbar-brand" href="#"
+        ><img src="../../img/logo.png" height="60"
+      /></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link mx-2 active" aria-current="page" href="#"
+              >Home</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="#">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-2" href="#">Pricing</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link mx-2 dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Company
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Blog</a></li>
+              <li><a class="dropdown-item" href="#">About Us</a></li>
+              <li><a class="dropdown-item" href="#">Contact us</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-  export default {
-    name: 'MyHeader',
-  } 
+export default {
+  name: "MyHeader",
+};
 </script>
+
+<style>
+#neubar {
+  background: #fff7ee;
+  box-shadow: 4px 6px 12px rgba(0, 0, 0, 0.4);
+}
+
+.nav-item .active {
+  border-radius: 6px;
+  background: #81b622;
+}
+#neubar .dropdown-menu a:hover {
+  color: #81b622;
+}
+#neubar .nav-item {
+  margin: auto 4px;
+}
+#neubar a {
+  padding-left: 12px;
+  padding-right: 12px;
+}
+#neubar .dropdown-menu {
+  background: #81b622;
+}
+a.navbar-brand {
+  color: #81b622;
+}
+</style>
