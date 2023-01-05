1f966e7b4b69505031e4377b6b4d1dd3ec448fe4
Clément CHAZALET <clementchazalet@gmail.com>
2023-01-05 11:00:33 -0500

    Add logo to result element

--- greenminded/src/components/result_components/BusResultElement.vue
+++ greenminded/src/components/result_components/BusResultElement.vue
@@ -1,74 +1,87 @@
 <template>
     <div id="result_element">
-        <div class="sub_element">
-            <label for="status" class="label_type">Departure:</label>
-            {{depart}}
+        <div class="row">
+            <div class="col-sm-1">
+                <img src="../../../img/1280px-Flixbus_201x_logo.svg.png" class="logo-transport-type">
+            </div>
+            <div class="col-sm-11">
+                <div class="sub_element">
+                    <label for="status" class="label_type">Departure:</label>
+                    {{ depart }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Arrival:</label>
+                    {{ arrival }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Departure Time:</label>
+                    {{ values.depatureTime }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Arrival Time:</label>
+                    {{ values.arrivalTime }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Price:</label>
+                    {{ values.price }} euros
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Duration:</label>
+                    {{ values.duration }} min
+                </div>
+                {{ values.msg }}
+            </div>
         </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Arrival:</label>
-            {{arrival}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Departure Time:</label>
-            {{values.depatureTime}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Arrival Time:</label>
-            {{values.arrivalTime}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Price:</label>
-            {{values.price}} euros
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Duration:</label>
-            {{values.duration}} min
-        </div>
-        {{values.msg}}
     </div>
 </template>
 
 <script>
-    export default {
-        name: "BusResultElement",
-        components:{
+export default {
+    name: "BusResultElement",
+    components: {
 
-        },
-        data(){
-            return{
+    },
+    data() {
+        return {
 
-            }
-        },
-        props:{
-            values: {String},
-            depart: String,
-            arrival: String
         }
+    },
+    props: {
+        values: { String },
+        depart: String,
+        arrival: String
     }
+}
 
 </script>
 
 <style scoped>
-    #result_element{
-        border-color: #81b622;
-        border-radius: 5px;
-        border-style: groove;
-        margin-top: 5px;
-        margin-left: 50px;
-        margin-right: 50px;
-        padding: 5px;
-        padding-left: 5%;
-    }
+#result_element {
+    border-color: #81b622;
+    border-radius: 5px;
+    border-style: groove;
+    margin-top: 5px;
+    margin-left: 50px;
+    margin-right: 50px;
+    padding: 5px;
+    padding-left: 5%;
+}
 
-    .sub_element{
-        display:inline-block;
-        width: 50%;
-    }
-    
-    .label_type{
-        width: 35%;
-        font-weight: 200;
-        font-style: oblique;
-        font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
-    }
+.sub_element {
+    display: inline-block;
+    width: 50%;
+}
+
+.label_type {
+    width: 35%;
+    font-weight: 200;
+    font-style: oblique;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+}
+
+
+.logo-transport-type {
+    margin-top: 10px;
+    width: 80px;
+}
 </style>
--- greenminded/src/components/result_components/TrainResultElement.vue
+++ greenminded/src/components/result_components/TrainResultElement.vue
@@ -1,71 +1,83 @@
 <template>
     <div id="result_element">
-        <div class="sub_element">
-            <label for="status" class="label_type">Departure:</label>
-            {{values.source}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Arrival:</label>
-            {{values.destination}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Departure Time:</label>
-            {{values.departureTime}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Arrival Time:</label>
-            {{values.arrivalTime}}
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">CO2 Emission:</label>
-            {{values.co2Emission}} g
-        </div>
-        <div class="sub_element">
-            <label for="status" class="label_type">Duration:</label>
-            {{values.duration}} min
+        <div class="row">
+            <div class="col-sm-1">
+                <img src="../../../img/Logo_SNCF_2011.svg.png" class="logo-transport-type">
+            </div>
+            <div class="col-sm-11">
+                <div class="sub_element">
+                    <label for="status" class="label_type">Departure:</label>
+                    {{ values.source }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Arrival:</label>
+                    {{ values.destination }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Departure Time:</label>
+                    {{ values.departureTime }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Arrival Time:</label>
+                    {{ values.arrivalTime }}
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">CO2 Emission:</label>
+                    {{ values.co2Emission }} g
+                </div>
+                <div class="sub_element">
+                    <label for="status" class="label_type">Duration:</label>
+                    {{ values.duration }} min
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "ResultElement",
-        components:{
+export default {
+    name: "ResultElement",
+    components: {
 
-        },
-        data(){
-            return{
+    },
+    data() {
+        return {
 
-            }
-        },
-        props:{
-            values: {String}
         }
+    },
+    props: {
+        values: { String }
     }
+}
 
 </script>
 
 <style scoped>
-    #result_element{
-        border-color: #81b622;
-        border-radius: 5px;
-        border-style: groove;
-        margin-top: 5px;
-        margin-left: 50px;
-        margin-right: 50px;
-        padding: 5px;
-        padding-left: 5%;
-    }
+#result_element {
+    border-color: #81b622;
+    border-radius: 5px;
+    border-style: groove;
+    margin-top: 5px;
+    margin-left: 50px;
+    margin-right: 50px;
+    padding: 5px;
+    padding-left: 5%;
+}
 
-    .sub_element{
-        display:inline-block;
-        width: 50%;
-    }
-    
-    .label_type{
-        width: 35%;
-        font-weight: 200;
-        font-style: oblique;
-        font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
-    }
+.sub_element {
+    display: inline-block;
+    width: 50%;
+}
+
+.label_type {
+    width: 35%;
+    font-weight: 200;
+    font-style: oblique;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+}
+
+.logo-transport-type {
+    margin-top: 10px;
+    width: 80px;
+}
 </style>
