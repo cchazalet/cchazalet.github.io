bb69c1aeb7b6ddd363a4c186178bac060aebf767
Clément CHAZALET <clementchazalet@gmail.com>
2023-01-05 10:15:24 -0500

    carrousel ok

---
+++ greenminded/src/components/main_components/NewSearch.vue
@@ -0,0 +1,150 @@
+<template>
+    <section class="quick-search">
+        <form action="#" method="post" novalidate="novalidate">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3 col-sm-12 p-0">
+                        <input type="text" placeholder="Enter A City of Departure" v-model="departure"
+                            class="form-control search-slt">
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-12 p-0">
+                        <input type="text" placeholder="Enter A City of Arrival" v-model="arrival"
+                            class="form-control search-slt">
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-sm-12 p-0">
+                        <input type="date" v-model="date" class="form-control search-slt">
+                    </div>
+                    <div class="col-lg-1 col-md-3 col-sm-12 p-0">
+                        <input type="time" v-model="time" class="form-control search-slt">
+                    </div>
+                    <div class="col-lg-1 col-md-3 col-sm-12 p-0">
+                        <input name="submit" value="Search" class="btn btn-success
+                                                            btn-lg btn-block wrn-btn" type="submit"
+                            @click="searchIte()">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </section>
+    <section>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../../../img/slides/1.jpg" class="d-block w-100" alt="../../../img/slides/1.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../../img/slides/2.jpg" class="d-block w-100" alt="../../../img/slides/2.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../../img/slides/3.jpg" class="d-block w-100" alt="../../../img/slides/3.jpg">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+</template>
+<script>
+// import API from '../../plugins/axiosInstance'
+
+export default {
+    name: 'SearchElement',
+    data() {
+        return {
+            departure: '',
+            arrival: '',
+            date: NaN,
+            time: NaN,
+            price: 0,
+            duration: 0,
+            ecology: 0,
+        }
+    },
+    components: {
+
+    },
+    methods: {
+        searchIte() {
+            var reg1 = new RegExp('-', 'g')
+            this.$router.push({
+                name: 'result',
+                query: {
+                    'source': this.departure,
+                    'destination': this.arrival,
+                    'hour': this.time,
+                    'date': this.date.replace(reg1, ''),
+                    'price': this.price,
+                    'duration': this.duration,
+                    'ecology': this.ecology,
+                }
+            })
+        }
+    },
+}
+
+</script>
+
+
+<style scoped>
+/*search box css start here*/
+
+.quick-search {
+    padding: 25px 40px;
+    background: rgba(255, 255, 255, .7);
+}
+
+.search-sec {
+    padding: 25px 40px;
+}
+.search-slt {
+    display: block;
+    width: 100%;
+    font-size: 0.875rem;
+    line-height: 1.5;
+    color: #55595c;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    height: calc(3rem + 2px) !important;
+    border-radius: 0;
+}
+
+.wrn-btn {
+    width: 100%;
+    font-size: 16px;
+    font-weight: 400;
+    text-transform: capitalize;
+    height: calc(3rem + 2px) !important;
+    border-radius: 0;
+}
+
+@media (min-width: 992px) {
+    .search-sec {
+        position: relative;
+        top: -114px;
+        background: rgba(255, 255, 255, .7);
+    }
+}
+
+@media (max-width: 992px) {
+    .search-sec {
+        background: rgba(255, 255, 255, .7);
+    }
+}
+</style>
--- greenminded/src/components/main_components/Search.vue
+++ greenminded/src/components/main_components/Search.vue
@@ -7,20 +7,20 @@
                         <div class="col-md-6 col-sm-12">
                             <!--Search Component-->
                             <div class="quick-search">
-                                <div class = "row">
+                                <div class="row">
                                     <form role="form">
                                         <div class="col-md-6 col-sm-6 col-xs-6">
                                             <div class="form-group">
                                                 <label for="status">Departure</label>
-                                                <input type="text" placeholder="Enter A City" 
-                                                v-model="departure" class="form-control">
+                                                <input type="text" placeholder="Enter A City" v-model="departure"
+                                                    class="form-control">
                                             </div>
                                         </div>
                                         <div class="col-md-6 col-sm-6 col-xs-6">
                                             <div class="form-group">
                                                 <label for="status">Arrival</label>
-                                                <input type="text" placeholder="Enter A City" 
-                                                v-model="arrival" class="form-control">
+                                                <input type="text" placeholder="Enter A City" v-model="arrival"
+                                                    class="form-control">
                                             </div>
                                         </div>
                                         <div class="col-md-6 col-sm-6 col-xs-6">
@@ -43,7 +43,8 @@
                                         </div>
                                         <div class="col-md-9 col-sm-9 col-xs-9">
                                             <div class="form-group">
-                                                <input type="range" min="0" max="10" v-model="price" class="form-control">
+                                                <input type="range" min="0" max="10" v-model="price"
+                                                    class="form-control">
                                             </div>
                                         </div>
 
@@ -54,7 +55,8 @@
                                         </div>
                                         <div class="col-md-9 col-sm-9 col-xs-9">
                                             <div class="form-group">
-                                                <input type="range" min="0" max="10" v-model="duration" class="form-control">
+                                                <input type="range" min="0" max="10" v-model="duration"
+                                                    class="form-control">
                                             </div>
                                         </div>
 
@@ -65,7 +67,8 @@
                                         </div>
                                         <div class="col-md-9 col-sm-9 col-xs-9">
                                             <div class="form-group">
-                                                <input type="range" min="0" max="10" v-model="ecology" class="form-control">
+                                                <input type="range" min="0" max="10" v-model="ecology"
+                                                    class="form-control">
                                             </div>
                                         </div>
 
@@ -79,168 +82,298 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <!-- Carousel
+================================================== -->
+            <div id="myCarousel" class="carousel slide">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="../../../img/slides/1.jpg">
+                        <div class="container">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="../../../img/slides/2.jpg">
+                        <div class="container">
+                            <div class="carousel-caption">
 
-            </div>            
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="../../../img/slides/3.jpg">
+                        <div class="container">
+                            <div class="carousel-caption">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <form class="col-sm-12" id="searchForm">
+                    <div class="form-group col-sm-4 col-sm-offset-4">
+                        <div class="input-group input-group-lg center-block">
+                            <input type="text" class="form-control" placeholder="Search">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+                        </div>
+                    </div>
+                </form>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <i class="glyphicon glyphicon-chevron-left"></i>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <i class="glyphicon glyphicon-chevron-right"></i>
+                </a>
+            </div>
+            <!-- /.carousel -->
+
+            <div class="container">
+
+                <!-- FOOTER -->
+                <footer>
+                    <p class="pull-right"><a href="#">Back to top</a></p>
+                    <p>This Bootstrap layout is compliments of Bootply. · <a href="http://www.bootply.com/108847">Edit
+                            on Bootply.com</a></p>
+                </footer>
+
+            </div><!-- /.container -->
+            <!--
             <ul class="slides">
                 <li>
                     <img src="../../../img/slides/1.jpg" alt=""/>
                 </li>
-                <!-- <li>
+                <li>
                     <img src="../../../img/slides/2.jpg" alt=""/>
                 </li>
                 <li>
                     <img src="../../../img/slides/3.jpg" alt=""/>
-                </li> -->
-            </ul>
-
+                </li>
+            </ul>-->
         </div>
-    </div>
-    <div>
-        {{departure}}{{time}}{{date}}{{price}}
     </div>
 </template>
 
 <script>
-    // import API from '../../plugins/axiosInstance'
+// import API from '../../plugins/axiosInstance'
 
-    export default{
-        name: 'SearchElement',
-        data(){
-            return{
-                departure: '',
-                arrival: '',
-                date: NaN,
-                time: NaN,
-                price: 0,
-                duration: 0,
-                ecology: 0,
-            }
-        },
-        components:{
+export default {
+    name: 'SearchElement',
+    data() {
+        return {
+            departure: '',
+            arrival: '',
+            date: NaN,
+            time: NaN,
+            price: 0,
+            duration: 0,
+            ecology: 0,
+        }
+    },
+    components: {
 
-        },
-        methods:{
-            searchIte(){
-                var reg1 = new RegExp('-', 'g')
-                this.$router.push({
-                    name: 'result',
-                    query: {
-                        'source': this.departure,
-                        'destination': this.arrival,
-                        'hour': this.time,
-                        'date': this.date.replace(reg1,''),
-                        'price': this.price,
-                        'duration': this.duration,
-                        'ecology': this.ecology,
-                    }
-                })
-            }
-        },
-    }
+    },
+    methods: {
+        searchIte() {
+            var reg1 = new RegExp('-', 'g')
+            this.$router.push({
+                name: 'result',
+                query: {
+                    'source': this.departure,
+                    'destination': this.arrival,
+                    'hour': this.time,
+                    'date': this.date.replace(reg1, ''),
+                    'price': this.price,
+                    'duration': this.duration,
+                    'ecology': this.ecology,
+                }
+            })
+        }
+    },
+}
 
 </script>
 
 <style scoped>
-    #featured{
-        width: 100%;
-        background:#000;
-        position:relative;
-        margin:0;
-        padding:0;
+#featured {
+    width: 100%;
+    background: #000;
+    position: relative;
+    margin: 0;
+    padding: 0;
+}
+
+.flexslider {
+    padding: 0;
+    background: #fff;
+    position: relative;
+    zoom: 1;
+}
+
+.flex-caption {
+    zoom: 1;
+    bottom: 35px;
+    background-color: transparent;
+    color: #fff;
+    margin: 0;
+    padding: 25px 25px 25px 30px;
+    position: absolute;
+    left: 0;
+    right: 0;
+}
+
+.flex-caption h3 {
+    color: #fff;
+    letter-spacing: 1px;
+    margin-bottom: 8px;
+    text-transform: uppercase;
+}
+
+.flex-caption p {
+    margin: 0 0 15px;
+}
+
+.row .row,
+.row-fluid .row-fluid {
+    margin-bottom: 30px;
+}
+
+.row.nomargin,
+.row-fluid.nomargin {
+    margin-bottom: 0;
+}
+
+.quick-search {
+    padding: 25px 40px;
+    background: rgba(255, 255, 255, .7);
+}
+
+.form-group {
+    margin-bottom: 15px;
+}
+
+.form-control {
+    display: block;
+    width: 100%;
+    height: 34px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #555;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+    -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+    -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+}
+
+.btn {
+    text-align: center;
+    background: #47A447;
+    color: #fff;
+    border-color: #47A447;
+}
+
+.col-xs-3 .col-sm-3 .col-md-3 .col-xs-6 .col-sm-6 .col-md-6 .col-xs-9 .col-sm-9 .col-md-9 .col-sm-12 .col-md-12 {
+    position: relative;
+    min-height: 1px;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+
+.col-xs-3 {
+    width: 25%;
+    float: left;
+}
+
+.col-xs-6 {
+    width: 50%;
+    float: left;
+}
+
+.col-xs-9 {
+    width: 75%;
+    float: left;
+}
+
+label {
+    display: inline-block;
+    max-width: 100%;
+    margin-bottom: 5px;
+    font-weight: bold;
+    color: #555;
+}
+
+input[type=range] {
+    display: block;
+    width: 100%;
+}
+
+input[type="range" i] {
+    appearance: auto;
+    cursor: default;
+}
+
+/* CUSTOMIZE THE CAROUSEL
+-------------------------------------------------- */
+
+.carousel {
+    margin-bottom: 60px;
+}
+
+.carousel-control {
+    top: 76%;
+}
+
+.carousel-caption {
+    z-index: 10;
+}
+
+.carousel .item {
+    height: 500px;
+    background-color: #bbb;
+    overflow: hidden;
+}
+
+.carousel img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    height: 500px;
+}
+
+#searchForm {
+    position: absolute;
+    top: 40%;
+}
+
+/* RESPONSIVE CSS
+-------------------------------------------------- */
+@media (max-width: 768px) {
+
+    .carousel-inner>.item>img,
+    .carousel-inner>.item>a>img {
+        max-width: inherit;
     }
 
-    .flexslider {
-        padding:0;  
-        background: #fff; 
-        position: relative; 
-        zoom: 1;
+    .carousel-caption p {
+        margin-bottom: 20px;
+        font-size: 21px;
+        line-height: 1.4;
     }
-    .flex-caption {zoom: 1;
-    bottom: 35px; background-color: transparent; color: #fff; margin: 0; padding: 25px 25px 25px 30px; position: absolute; left: 0; right: 0;}
-    .flex-caption h3 {color: #fff; letter-spacing: 1px; margin-bottom: 8px; text-transform: uppercase;}
-    .flex-caption p {margin: 0 0 15px;}
-
-    .row .row,.row-fluid .row-fluid{
-        margin-bottom:30px;
-    }
-
-    .row.nomargin,.row-fluid.nomargin {
-        margin-bottom:0;
-    }
-    .quick-search {
-        padding: 25px 40px;
-        background: rgba(255,255,255,.7);
-    }
-
-    .form-group {
-        margin-bottom: 15px;
-    }
-
-    .form-control {
-        display: block;
-        width: 100%;
-        height: 34px;
-        padding: 6px 12px;
-        font-size: 14px;
-        line-height: 1.42857143;
-        color: #555;
-        background-color: #fff;
-        background-image: none;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-        box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-        -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
-        -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-        transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-    }
-
-    .btn{text-align: center;
-        background: #47A447;
-        color: #fff;
-        border-color: #47A447;
-    }
-
-    .col-xs-3 .col-sm-3 .col-md-3 .col-xs-6 .col-sm-6 .col-md-6 .col-xs-9 .col-sm-9 .col-md-9 .col-sm-12 .col-md-12{
-        position: relative;
-        min-height: 1px;
-        padding-left: 15px;
-        padding-right: 15px;
-    }
-
-    .col-xs-3 {
-        width: 25%;
-        float: left;
-    }
-    .col-xs-6 {
-        width: 50%;
-        float: left;
-    }
-
-    .col-xs-9 {
-        width: 75%;
-        float: left;
-    }
-
-    label {
-        display: inline-block;
-        max-width: 100%;
-        margin-bottom: 5px;
-        font-weight: bold;
-        color: #555;
-    }
-
-    input[type=range] {
-        display: block;
-        width: 100%;
-    }
-
-    input[type="range" i] {
-        appearance: auto;
-        cursor: default;
-    }
-
-
-
-
+}
 </style>
