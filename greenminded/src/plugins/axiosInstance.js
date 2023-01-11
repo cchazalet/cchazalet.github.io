fa6aed782aac85b7925404af7be8d2cc5f1ac5a2
Clément CHAZALET <clementchazalet@gmail.com>
2023-01-11 10:28:58 -0500

    Change port to 8080

--- greenminded/src/plugins/axiosInstance.js
+++ greenminded/src/plugins/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://120.26.198.0', //url backend
-    timeout: 30000
+    baseURL: 'http://120.26.198.0:8080', //url backend
+    timeout: 30000  
 })
 
 export default API
