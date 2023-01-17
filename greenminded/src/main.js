07b6414faacd10f877b84bf379d71dc4e1886342
LI ZEHAN <lzh1532805942@gmail.com>
2023-01-18 02:58:32 +0800

    add first page background

--- greenminded/src/main.js
+++ greenminded/src/main.js
@@ -3,16 +3,15 @@ import App from './App.vue'
 import router from './router/index'
 import axios from '@/plugins/axiosInstance.js'
 
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 //import "bootstrap"
+import "@/assets/css/bootstrap.min.css"
 import "@/assets/css/animate.min.css"
-//import "./assets/css/bootstrap.min.css"
 import "@/assets/css/jquery.nouislider.min.css"
 import "@/assets/css/owl.carousel.css"
 import "@/assets/css/owl.theme.default.css"
 import "@/assets/css/style.css"
 import "@/assets/css/sweetalert.css"
-
 import "@/assets/css/font.css"
 
 
